febddc15eeb6888c2b87e75745878bf2116bac51
Jakub Szurek <jakub.szuek10@gmail.com>
2026-05-30 21:22:35 +0200

    Added responsivity

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,9 @@
 "use strict";
+const verticalMenu = document.getElementById('vertical-menu');
+const horizontalMenu = document.getElementById('horizontal-menu');
+verticalMenu.style.top = `${horizontalMenu.offsetHeight - horizontalMenu.getBoundingClientRect().top + 10}px`;
+verticalMenu.style.maxHeight = `${verticalMenu.querySelector('a').offsetHeight * 4 + 40}px`;
+window.addEventListener('resize', (e) => {
+    verticalMenu.style.top = `${horizontalMenu.offsetHeight - horizontalMenu.getBoundingClientRect().top + 10}px`;
+    verticalMenu.style.maxHeight = `${verticalMenu.querySelector('a').offsetHeight * 4 + 50}px`;
+});
